4ae0b9a5170744f7c115cb7a11133af9014af6e3
ariwijayaputra <62342206+ariwijayaputra@users.noreply.github.com>
2021-09-10 13:23:20 +0800

    Update Credentials.h
    commenting credential

--- Credentials.h
+++ Credentials.h
@@ -2,22 +2,22 @@
 #define ARDUINO_CREDENTIALS_H
 
 /* WiFi Credentials to connect Internet */
-#define STA_SSID "A4Y Family"//A4Y Family
-#define STA_PASS "DK1623GC"
+#define STA_SSID " " // wifi name
+#define STA_PASS " " // wifi password
 
 /* Provide MQTT broker credentials as denoted in maqiatto.com. */
 #define MQTT_BROKER       "maqiatto.com"
 #define MQTT_BROKER_PORT  1883
-#define MQTT_USERNAME     "aryanathaaa@gmail.com"
-#define MQTT_KEY          "Ariwijaya1"
+#define MQTT_USERNAME     ""//example : aryanathaaa@gmail.com
+#define MQTT_KEY          ""//your maqiatto password
 
 
 /* Provide topic as it is denoted in your topic list. 
- * For example mine is : cadominna@gmail.com/topic1
+ * For example mine is : aryanathaaa@gmail.com/motion. <- motion is topic name
  * To add topics, see https://www.maqiatto.com/configure
  */
-#define TOPIC    "aryanathaaa@gmail.com/motion"
-#define STATUS  "aryanathaaa@gmail.com/status"
-#define JUMLAH  "aryanathaaa@gmail.com/jumlah"
+#define TOPIC   " "//ex : aryanathaaa@gmail.com/motion
+#define STATUS  " "//ex : aryanathaaa@gmail.com/status
+#define JUMLAH  " "//ex : aryanathaaa@gmail.com/jumlah
 
 #endif /* ARDUINO_CREDENTIALS_H */
