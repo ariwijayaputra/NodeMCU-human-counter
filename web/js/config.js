be5ac091c71245bb151319b6e7a316985d8822e3
ariwijayaputra <62342206+ariwijayaputra@users.noreply.github.com>
2021-09-10 19:49:03 +0800

    Update config.js
    adding topic

--- web/js/config.js
+++ web/js/config.js
@@ -1,7 +1,8 @@
 
 host = 'maqiatto.com';	// hostname or IP address
 port = 8883;
-topic = '';		// topic to subscribe to. status masuk keluar. 'example: aryanathaaa@gmail.com/status'
+topic = '';		// topic to subscribe to. main topic. 'example: aryanathaaa@gmail.com/status'
+STATUS = '';  // topic to subscribe to. status masuk keluar. 'example: aryanathaaa@gmail.com/status'
 jumlah = '';	// topic to subscribe to. jumlah orang. ex: 'aryanathaaa@gmail.com/jumlah'
 useTLS = false;
 username = "";//maqiatto username ex: 'aryanathaaa@gmail.com/'
