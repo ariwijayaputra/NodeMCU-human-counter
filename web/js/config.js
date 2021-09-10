20879bf404b00d3270f22e3bf3cdf857493e14a6
ariwijayaputra <62342206+ariwijayaputra@users.noreply.github.com>
2021-09-10 19:34:59 +0800

    Update config.js

--- web/js/config.js
+++ web/js/config.js
@@ -1,10 +1,10 @@
 
 host = 'maqiatto.com';	// hostname or IP address
 port = 8883;
-topic = '';		// topic to subscribe to. status masuk keluar. example: aryanathaaa@gmail.com/status
+topic = '';		// topic to subscribe to. status masuk keluar. 'example: aryanathaaa@gmail.com/status'
 jumlah = '';	// topic to subscribe to. jumlah orang. ex: 'aryanathaaa@gmail.com/jumlah'
 useTLS = false;
-username = "";//maqiatto username ex: aryanathaaa@gmail.com/
+username = "";//maqiatto username ex: 'aryanathaaa@gmail.com/'
 password = "" ;//maqiatto password
 
 cleansession = true;
