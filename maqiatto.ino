e0593427868a4f09db23ca06935ae7dc95affdab
ariwijayaputra <62342206+ariwijayaputra@users.noreply.github.com>
2021-09-10 13:12:24 +0800

    Update maqiatto.ino
    reducing delay to blink lcd
    delete useless comment
    adding description comment

--- maqiatto.ino
+++ maqiatto.ino
@@ -48,53 +48,89 @@ void setup()
   lcdsetup();
   Serial.setDebugOutput(true);
   pinMode(pirSensor, INPUT);
-  WiFiBegin(STA_SSID, STA_PASS);
-  MQTTBegin();
+  WiFiBegin(STA_SSID, STA_PASS);//mengkoneksi wifi
+  MQTTBegin();//memulai koneksi MQTT
 
   Serial.println("Preparing sensor...");
   lcd.setCursor(0, 0);
   lcd.print("Preparing sensor");
   delay(4000);
   lcd.noBacklight();
-  delay(500);
+  delay(300);
   lcd.backlight();
-  delay(500);
+  delay(300);
   lcd.noBacklight();
-  delay(500);
+  delay(300);
   lcd.backlight();
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Sensor Ready");
-
 }
 
 void loop()
 {
   MQTTLoop();
-  //  if (MQTTPublish(TOPIC, "test maqiatto!"))
-  //  {
-  //    Serial.printf("MQTTPublish was succeeded.\n");
-  //  }
   in = int(getIndoor());
   out = int(getOutdoor());
   if ((in == 1 || out == 1) && timer == 0) {
+    /*
+    Kondisi untuk mereset timer timeout. timer menyala (timer = 1) jika
+    salah satu sensor mendeteksi gerakan dan jika timer dalam keadaan mati.
+    variabel timeout dalam hal ini akan terus melakukan increment
+    oleh karena itu, timeout harus di reset pada kondisi ini.
+    */
     timeout = 0;
     timer = 1;
   }
   if (timeout > 3000) {
+    /*
+    kondisi untuk mereset nilai sensor dan mematikan timer. 
+    dilakukan apabila salah satu sensor telah mendeteksi 
+    gerakan namun tidak ada deteksi gerakan dari sensor
+    lainnya dalam rentang waktu 3 detik (3000ms).
+    Seseorang harus melewati kedua buah sensor dalam rentang
+    waktu 3 detik untuk bisa dideteksi telah melewati pintu.
+    ubah timeout > 3000 jika ingin menambah/mengurangi waktu tunggu
+    sensor.
+    */
     writeIndoor(0);
     writeOutdoor(0);
     timer = 0;
   }
   current = getPayload();
+  /*
+  current adalah variabel untuk menyimpan data sensor mana yang saat ini
+  mendeteksi gerakan.
+  current = 1 jika indoor yang baru saja mendeteksi gerakan.
+  current = 2 jika outdoor yang baru saja mendeteksi gerakan.
+  */
   if (in == 1 && out == 1) {
+    /*
+      jika sensor dalam dan luar mendeteksi gerakan maka nilai in dan
+      out adalah 1. kondisi diatas digunakan apabila kedua sensor mendeteksi
+      gerakan sebelum 3 detik. jika kondisi ini TRUE maka sensor mendeteksi
+      ada seseorang yang telah melewati pintu, namun masih belum diketahui arahnya
+      apakah orang tersebut masuk atau keluar.
+    */
     if (last == 2 && current == 1) {
+      /*
+        jika current == 1 (indoor yang saat ini mendeteksi gerakan). 
+        dan last == 2 (outdoor yang sebelumnya mendeteksi gerakan)
+        maka arah orang tersebut:
+        outdoor -> indoor (Masuk)
+      */
       Serial.println("==== MASUK ======");
       MQTTPublish(STATUS, "1");
       jumlahOrang++;
       lcd.clear();
     }
     else if (last == 1 && current == 2) {
+      /*
+        jika current == 2 (outdoor yang saat ini mendeteksi gerakan). 
+        dan last == 1 (indoor yang sebelumnya mendeteksi gerakan)
+        maka arah orang tersebut:
+        indoor -> outdoor (Keluar)
+      */
       Serial.println("==== KELUAR ======");
       MQTTPublish(STATUS, "2");
       jumlahOrang--;
@@ -103,23 +139,26 @@ void loop()
     
     lcd.setCursor(0, 1);
     lcd.print("Jumlah = ");
+    lcd.print(jumlahOrang);
+    
+    /*
+    inisiasi char jumlah untuk publish jumlahOrang
+    ke MQTT karena MQTTPublish hanya menerima type *char
+    */
     char jumlah[5];
     sprintf(jumlah, "%d", jumlahOrang);
-    lcd.print(jumlahOrang);
     MQTTPublish(JUMLAH, jumlah);
-    writeIndoor(0);
-    writeOutdoor(0);
-    timer = 0;
+    
+    writeIndoor(0);// reset data (indoor tidak mendeteksi gerakan)
+    writeOutdoor(0);// reset data (outdoor tidak mendeteksi gerakan)
+    timer = 0;//reset timer =  mati
   }
   last = current;
 
-
-
   if (interval >= 500) {
+    //membaca nilai pir dan mempublish data setiap 500ms
     pir();
     interval = 0;
-
-    Serial.println(getPayload());
   }
 
 }
